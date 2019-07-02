6d7e281a8cd2dc0c7a5f7189f471a62682800180
Kuralamudhu <51950263+Kuralamudhu@users.noreply.github.com>
2019-07-02 11:27:03 +0530

    Create python23.py

---
+++ python23.py
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void) {
+        int o,y,u,p=0,h,k;
+        scanf("%d %d",&h,&k);
+        for(i=h+1;i<k;,i++){
+        o=y;
+        p=0;
+        while(o>0){
+                u=o%10;
+                p=p+(u*u*u);
+                o=o/10;
+        }
+        if(y==p)
+        printf("%d" ,y);}
+        return 0;
+}
+             
